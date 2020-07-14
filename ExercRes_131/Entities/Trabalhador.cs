90ef23114f6b4d77fd088edd715a54d97a5aea42
marceloAPP <marcelo_freeday@yahoo.com.br>
2020-07-13 22:28:52 -0300

    Implementando as entidades Trabalhador/TrabalhadorTerceirizado

---
+++ ExercRes_131/Entities/Trabalhador.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercRes_131.Entities
+{
+    class Trabalhador
+    {
+        public string Nome { get; set; }
+        public int Horas { get; set; }
+        public double ValorHora { get; set; }
+
+        public Trabalhador()
+        {
+        }
+
+        public Trabalhador(string nome, int horas, double valorHora)
+        {
+            Nome = nome;
+            Horas = horas;
+            ValorHora = valorHora;
+        }
+
+        public virtual double Pagamento() {
+
+            return ValorHora * Horas;
+        }
+
+    }
+}
---
+++ ExercRes_131/Entities/TrabalhadorTerceirizado.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercRes_131.Entities
+{
+    class TrabalhadorTerceirizado : Trabalhador
+    {
+        public double CobrancaAdicional { get; set; }
+
+        public TrabalhadorTerceirizado()
+        {
+        }
+
+        public TrabalhadorTerceirizado(string nome, int horas, double valorHora, double cobrancaAdicional)
+            : base(nome, horas, valorHora)
+        {
+            CobrancaAdicional = cobrancaAdicional;
+        }
+
+        public override double Pagamento()
+        {
+            double valor = ValorHora * Horas;
+
+            valor += valor + (CobrancaAdicional * 110.0) / 100.0;
+
+            return valor;
+        }
+
+    }
+}
