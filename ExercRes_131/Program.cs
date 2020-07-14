6c8c3849147b57605242d12fe2285c6e37d72b1d
marceloAPP <marcelo_freeday@yahoo.com.br>
2020-07-13 21:17:10 -0300

    Primeiro commit incluir .gitigonore ao projeto

---
+++ ExercRes_131/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercRes_131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
